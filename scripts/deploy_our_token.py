f3779745ed479e4ad45e2e32c5d45905647a72ba
DieKant <olmastroni.fedro2000@gmail.com>
2021-10-20 19:30:07 +0200

    fix of initial supply 2, aggiunta metodo alternativo per la scelta dell'initial supply

--- scripts/deploy_our_token.py
+++ scripts/deploy_our_token.py
@@ -1,11 +1,17 @@
 from scripts.helpful_scripts import get_account
 from brownie import OurToken, network, config
+from web3 import Web3
 
+# metodo PatrickCollins1
+initial_supply = Web3.toWei(21000000, "ether")
 
 def deploy_our_token():
     account = get_account()
     our_token = OurToken.deploy(
-        config["networks"][network.show_active()]["initialSupply"],
+        # metodo Fedro1
+        # config["networks"][network.show_active()]["initialSupply"]
+        # metodo PatrickCollins2
+        initial_supply,
         {"from": account},
         publish_source=config["networks"][network.show_active()].get("verify", False),
     )
